c7791895a73ed71687a2b88cc7eff3061a3362c7
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-05-05 13:48:38 +0530

    Assignment

---
+++ 01.python assignment1/integer.py
@@ -0,0 +1,14 @@
+# Ask the user
+
+Movie_Name = input("Enter Movie Name :- ")
+Viewer_Name  = input("Enter Veiwer Your  Name :- ")
+Seat_Number  = input("Enter Seat No :- ")
+Show_Time  = input("Enter Show Time :- ")
+
+print("_"*50)
+print(f"Movies name :- {Movie_Name}")
+print(f"Viewer Name :- {Viewer_Name}")
+print(f"Seat No :- {Seat_Number}")
+print(f"Show Time :- {Show_Time}")
+print("Enjoy the show! ")
+
