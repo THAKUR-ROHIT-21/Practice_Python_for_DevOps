26a2723ff2ff14311df86d615d98ef9d317f79e8
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-05-26 16:56:35 +0530

    Assignment third

--- 03_python_assignment_03/19__question.py
+++ 03_python_assignment_03/19__question.py
@@ -1,10 +1,23 @@
 # 19.Write a Python program to generate the pattern of the letter H.
 
+# n = 7 
+# for i in range(n):
+#     if i == n // 2:
+#         print('*' * n) 
+#     else:
+#         print('*' + ' ' * (n - 2) + '*')
 
-n = 7 
+
+# Secound Method 
+
+
+n = int(input("Enter Your Number :- "))
 for i in range(n):
-    if i == n // 2:
-        print('*' * n) 
-    else:
-        print('*' + ' ' * (n - 2) + '*')
+    for j in range(n):
+        if j==0 or j==n-1 or i==n//2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print( )
+
         
